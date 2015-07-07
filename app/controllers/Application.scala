17d5222e135dec371fb793a55f599b7a569c7047
Anindya Roy <thunderstormz.roy@gmail.com>
2015-07-07 17:54:34 +0530

    testing raw data

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,8 @@
 package controllers
 
+import play.api.libs.json.JsValue
 import play.api.mvc._
+import play.api.libs.json._
 
 class Application extends Controller {
 
@@ -9,7 +11,46 @@ class Application extends Controller {
   }
 
   def home = Action {
-    Ok(views.html.index("This is my new home!"))
+
+    val json: JsValue = Json.parse("""
+{
+  "name" : "Watership Down",
+  "location" : {
+    "lat" : 51.235685,
+    "long" : -1.309197
+  },
+  "residents" : [ {
+    "name" : "Fiver",
+    "age" : 4,
+    "role" : null
+  }, {
+    "name" : "Bigwig",
+    "age" : 6,
+    "role" : "Owsla"
+  } ]
+}
+                                   """)
+
+    val lat=  (json  \"location"\ "lat").get
+    val marks =(json \\ "name")
+    var n: Int =  1
+    var total=0
+    var end=0
+    var t=1
+
+    while (((json \"residents")(n-1))!=null){
+      n+=1
+    }
+
+    while(t!=n){
+        var x=(json \ "residents")(t-1).get
+      val y = (x \ "age").get
+      end+= y.toString().toInt
+      println(end)
+      t+=1
+    }
+   // val nice = (json \ "residents")(1).get
+    Ok(views.html.index(end.toString()))
   }
 
 }
